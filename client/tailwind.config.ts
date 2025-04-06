42a84ddc717648849d9f58bf39cda5d8417f9112
Donatus Prince <donatusprince@gmail.com>
2025-04-06 21:00:13 +0100

    refactor: added wallet-ui styling to global css

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -6,8 +6,6 @@ export default {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@wallet-ui/react/dist/**/*.{js,ts,jsx,tsx}',
-    './node_modules/@wallet-ui/tailwind/**/*.{js,ts,jsx,tsx,css}',
   ],
   theme: {
     extend: {
